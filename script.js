63c6dd27c8932be1fa56db522730f2df6ba100f6
SamuSan <sambminns@gmail.com>
2014-05-14 16:49:28 +1200

    trying to get Marcs stuff fixed

--- script.js
+++ script.js
@@ -36,93 +36,93 @@ kpsApp.config(function($routeProvider) {
             controller: 'monitorController'
         });
 });
-// Array.prototype.contains = function(obj) {
-//     var i = this.length;
-//     while (i--) {
-//         if (this[i].ID === obj) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-// function expenditure(data){
-// 		var expenditure = 0;
-// 		for (var i = data.simulation.mail.length - 1; i >= 0; i--) {
-// 			var item = data.simulation.mail[i];
-// 			var origin = "";
-// 			if(NZ.contains(item.from)){
-// 				origin = "New Zealand";
-// 			}
-// 			else{
-// 				origin = item.from;
-// 			}
+Array.prototype.contains = function(obj) {
+     var i = this.length;
+     while (i--) {
+         if (this[i].ID === obj) {
+             return true;
+         }
+     }
+     return false;
+}
+function expenditure(data){
+		var expenditure = 0;
+		for (var i = data.simulation.mail.length - 1; i >= 0; i--) {
+			var item = data.simulation.mail[i];
+			var origin = "";
+			if(NZ.contains(item.from)){
+				origin = "New Zealand";
+			}
+			else{
+				origin = item.from;
+			}
 
-// 			var prices = data.simulation.price;
-// 			for (var x = prices.length - 1; x >= 0; x--) {
-// 				var currPrice = prices[x];
-// 				if(prices[x].to ===  item.to){
-// 					if(prices[x].from === origin){
-// 						if(prices[x].priority === item.priority){
-// 							price = prices[x];
-// 							var weightCost = price.weightcost * item.weight;
-// 							var volumeCost = price.volumecost * item.volume;
-// 							revenue+=(weightCost+volumeCost);
-// 						}
-// 					}
-// 				}
+			var prices = data.simulation.price;
+			for (var x = prices.length - 1; x >= 0; x--) {
+				var currPrice = prices[x];
+				if(prices[x].to ===  item.to){
+					if(prices[x].from === origin){
+						if(prices[x].priority === item.priority){
+							price = prices[x];
+							var weightCost = price.weightcost * item.weight;
+							var volumeCost = price.volumecost * item.volume;
+                            expenditure+=(weightCost+volumeCost);
+						}
+					}
+				}
 				
-// 			};
+			};
 
 
 
-// 		};
-// 		expenditure.toFixed(2).replace(/./g, function(c, i, a) {
-//    			 return i && c !== "." && !((a.length - i) % 3) ? ',' + c : c;
-// 		});
-// 		return expenditure;
-// 		};
+		};
+		expenditure.toFixed(2).replace(/./g, function(c, i, a) {
+    			 return i && c !== "." && !((a.length - i) % 3) ? ',' + c : c;
+		});
+		return expenditure;
+		};
 
-// function revenue(data){
-// 		var revenue = 0;
-// 		for (var i = data.simulation.mail.length - 1; i >= 0; i--) {
-// 			var item = data.simulation.mail[i];
-// 			var origin = "";
-// 			if(NZ.contains(item.from)){
-// 				origin = "New Zealand";
-// 			}
-// 			else{
-// 				origin = item.from;
-// 			}
+function revenue(data, NZ){
+		var revenue = 0;
+		for (var i = data.simulation.mail.length - 1; i >= 0; i--) {
+			var item = data.simulation.mail[i];
+			var origin = "";
+			if(NZ.contains(item.from)){
+				origin = "New Zealand";
+			}
+			else{
+				origin = item.from;
+			}
 
-// 			var prices = data.simulation.price;
-// 			for (var x = prices.length - 1; x >= 0; x--) {
-// 				var currPrice = prices[x];
-// 				if(prices[x].to ===  item.to){
-// 					if(prices[x].from === origin){
-// 						if(prices[x].priority === item.priority){
-// 							price = prices[x];
-// 							var weightCost = price.weightcost * item.weight;
-// 							var volumeCost = price.volumecost * item.volume;
-// 							revenue+=(weightCost+volumeCost);
-// 						}
-// 					}
-// 				}
+			var prices = data.simulation.price;
+			for (var x = prices.length - 1; x >= 0; x--) {
+				var currPrice = prices[x];
+				if(prices[x].to ===  item.to){
+					if(prices[x].from === origin){
+						if(prices[x].priority === item.priority){
+							price = prices[x];
+							var weightCost = price.weightcost * item.weight;
+							var volumeCost = price.volumecost * item.volume;
+							revenue+=(weightCost+volumeCost);
+						}
+					}
+				}
 				
-// 			};
+			};
 
 
 
-// 		};
-// 		revenue.toFixed(2).replace(/./g, function(c, i, a) {
-//    			 return i && c !== "." && !((a.length - i) % 3) ? ',' + c : c;
-// 		});
-// 		return revenue;
-// 		};
+		};
+		revenue.toFixed(2).replace(/./g, function(c, i, a) {
+    			 return i && c !== "." && !((a.length - i) % 3) ? ',' + c : c;
+		});
+		return revenue;
+		};
 
 // create the controller and inject Angular's $scope
 kpsApp.controller('mainController', function($scope, $http) {
-
-	$http.get("http://localhost:8000/data/nationalCities.json").success(function(data){
+var NZ;
+	$http.get("/data/nationalCities.json").success(function(data){
 
 		NZ = data.NewZealand.cities;
 
@@ -132,10 +132,10 @@ kpsApp.controller('mainController', function($scope, $http) {
 	});
 
 
-	$http.get("http://localhost:8000/data/master_simulation.json").success(function(data){
+	$http.get("data/master_simulation.json").success(function(data){
 
 		$scope.numItems = data.simulation.mail.length;
-		var rev = revenue(data);
+		var rev = revenue(data, NZ);
 		var exp = expenditure(data);
 		var time = deliveryTimes(data);
 		
